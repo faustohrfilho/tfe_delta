1ad64f413291c436bd8c34f700d7667bbb70a709
Augusto <augustoalfafa@gmail.com>
2020-12-05 15:47:34 -0300

    Adicionando pagina inicial

--- tfe_delta/polls/views.py
+++ tfe_delta/polls/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    text = 'Pagina Inicial'
+    context = {
+        'text': text,
+    }
+    return render(request,'polls/index.html',context)
